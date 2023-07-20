461911d6853c91b9584dc6d61a18c0e6a86f3c0d
jinvvoo <jinvvookim@gmail.com>
2023-07-21 08:57:33 +0900

    chap02 complete

--- 01_lecture-source/chap02-mapping-lecture-source/src/main/java/com/ohgiraffers/mapping/section03/compositekey/subsection01/embeddedid/LikeBookService.java
+++ 01_lecture-source/chap02-mapping-lecture-source/src/main/java/com/ohgiraffers/mapping/section03/compositekey/subsection01/embeddedid/LikeBookService.java
@@ -1,10 +1,28 @@
 package com.ohgiraffers.mapping.section03.compositekey.subsection01.embeddedid;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class LikeBookService {
+
+    private LikeRepository likeRepository;
+
+    public LikeBookService(LikeRepository likeRepository) {
+        this.likeRepository = likeRepository;
+    }
+
+    @Transactional
     public void generateLikeBook(LikeDTO likeInfo) {
+
+        Like like = new Like(
+                new LikeCompositeKey(
+                        new LikedMemberNo(likeInfo.getLikedMemberNo()),
+                        new LikedBookNo(likeInfo.getLikedBookNo())
+                )
+        );
+
+        likeRepository.save(like);
 
     }
 }
